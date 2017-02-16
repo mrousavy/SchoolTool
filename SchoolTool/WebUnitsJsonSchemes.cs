fc67af7a53ffdf074876fd13ee642b656c8d5aa1
mrousavy <marcrousavy@hotmail.com>
2017-02-16 14:16:25 +0100

    Added API PDF and Logout JSON Query

--- SchoolTool/WebUnitsJsonSchemes.cs
+++ SchoolTool/WebUnitsJsonSchemes.cs
@@ -1,23 +1,27 @@
 namespace SchoolTool {
+    //All JSON Queries from the WebUntis API translated to C#
     public struct WebUnitsJsonSchemes {
-
+        //Authenticate the given user and start a session
         public class Authentication {
-            public int id;
+            public string id;
             public string method = "authenticate";
             public @params @params;
             public string password;
             public string jsonrpc = "2.0";
         }
 
+        public class Logout {
+            public string id;
+            public string method = "logout";
+            public @params @params;
+            public string jsonrpc = "2.0";
+        }
+
+        //Parameter Class for JSON Query Method Parameters
         public class @params {
             public string user = "ANDROID";
             public string password;
             public string client;
-
-            public @params(string client, string password) {
-                this.password = password;
-                this.client = client;
-            }
         }
     }
 }
