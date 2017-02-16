6072e44fa6aedf63d0a8378b3c59dad7dc630349
mrousavy <marcrousavy@hotmail.com>
2017-02-16 14:56:29 +0100

    Reformatted JSON Queries

--- SchoolTool/WebUnitsJsonSchemes.cs
+++ SchoolTool/WebUnitsJsonSchemes.cs
@@ -7,15 +7,16 @@ namespace SchoolTool {
             public string id;
             public string method;
             public string jsonrpc = "2.0";
+            //public @params @params;
 
             public class @params { }
         }
         //Parameter Class for JSON Query Method Parameters
-        public class @params {
-            public string user = "ANDROID";
-            public string password;
-            public string client;
-        }
+        //public class @params {
+        //    public string user = "ANDROID";
+        //    public string password;
+        //    public string client;
+        //}
 
         //Response or Result from a Query
         public class result {
@@ -29,30 +30,31 @@ namespace SchoolTool {
         //Authenticate the given user and start a session
         public class Authentication : WebUntisQuery {
             public string id;
-            public string method = "authenticate";
-            public @params @params;
-            public string password;
+            public readonly string method = "authenticate";
+
+            public class @params {
+                public string user = "ANDROID";
+                public string password;
+                public string client;
+            }
         }
 
         //End the session
         public class Logout : WebUntisQuery {
             public string id;
-            public string method = "logout";
-            public @params @params;
+            public readonly string method = "logout";
         }
 
         //Get list of teachers
         public class Teachers : WebUntisQuery {
             public string id;
-            public @params @params = new @params();
             public readonly string method = "getTeachers";
         }
 
         //Get list of students
         public class Students : WebUntisQuery {
             public string id;
-            public string method = "getStudents";
-            public @params @params;
+            public readonly string method = "getStudents";
         }
         #endregion
     }
