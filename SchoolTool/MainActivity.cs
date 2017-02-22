8ca39e41be65bebcd653341119e5c250bf061b2f
mrousavy <marcrousavy@hotmail.com>
2017-02-22 09:31:08 +0100

    Added WebUntis API from NuGet

--- SchoolTool/MainActivity.cs
+++ SchoolTool/MainActivity.cs
@@ -1,17 +1,21 @@
 ﻿using Android.App;
 using Android.OS;
+using WebUntisSharp;
 
 namespace SchoolTool {
     [Activity(Label = "SchoolTool", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity {
+        private WebUntis _untis;
+
         protected override void OnCreate(Bundle bundle) {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-
-            WebUnitsHelper.GetTeachers(8);
         }
+
+
+        //TODO: Login Method &  [_untis = new WebUntis();]  Login
     }
 }
 
