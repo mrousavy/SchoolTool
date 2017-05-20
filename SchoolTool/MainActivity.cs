386b29c2683be4ecfb539a269a16a87a8d61f914
dmaniak <dmaniak@student.tgm.ac.at>
2017-05-20 12:47:05 +0200

    Added Loading Screen, Colors, Styles, English & German Strings and rewrote Login Screen

--- SchoolTool/MainActivity.cs
+++ SchoolTool/MainActivity.cs
@@ -10,24 +10,12 @@ namespace SchoolTool
     [Activity(Label = "SchoolTool", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : AppCompatActivity
     {
-        private WebUntis _untis;
-
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            SetContentView(Resource.Layout.Main);
-
-            //Get Button from View
-            Button btn = FindViewById<Button>(Resource.Id.btnTest);
-            //Set On Click Handler
-            btn.Click += delegate
-            {
-                //_untis = new WebUntis(username, password, schoolUrl, client);
-                Toast.MakeText(this, "Test", ToastLength.Short).Show();
-            };
+            // Changed for testing purposes
+            SetContentView(Resource.Layout.Loading);
         }
-
-        //TODO: Login Method &  [_untis = new WebUntis();]  Login
     }
 }
