0a0de1e4e25e0addede526512795f6fb0266fa0d
mrousavy <mrousavy>
2017-05-30 06:13:37 +0200

    Updated Activity files

---
+++ SchoolTool/LoginActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SchoolTool {
+    [Activity(Label = "LoginActivity")]
+    public class LoginActivity : Activity {
+        protected override void OnCreate(Bundle savedInstanceState) {
+            // Has to be set before Activity is created
+            SetTheme(Resource.Style.SchoolToolDefault);
+
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Login);
+            // Create your application here
+        }
+    }
+}
--- SchoolTool/MainActivity.cs
+++ SchoolTool/MainActivity.cs
@@ -5,20 +5,17 @@ using Android.Views;
 using Android.Widget;
 using WebUntisSharp;
 
-namespace SchoolTool
-{
+namespace SchoolTool {
     [Activity(Label = "SchoolTool", MainLauncher = true, Icon = "@drawable/logo")]
-    public class MainActivity : AppCompatActivity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
+    public class MainActivity : AppCompatActivity {
+        protected override void OnCreate(Bundle bundle) {
             // Has to be set before Activity is created
             SetTheme(Resource.Style.SchoolToolDefault);
 
             base.OnCreate(bundle);
 
             //TODO: Testing Purposes            
-            SetContentView(Resource.Layout.Settings);
+            SetContentView(Resource.Layout.Loading);
         }
     }
 }
