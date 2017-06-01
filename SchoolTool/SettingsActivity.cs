b1453d18f22df9eb2cc8a1f6fec040a5cc642487
mrousavy <mrousavy>
2017-06-01 12:12:02 +0100

    New Settings Activity

--- SchoolTool/SettingsActivity.cs
+++ SchoolTool/SettingsActivity.cs
@@ -1,66 +1,66 @@
-using Android.App;
-using Android.OS;
-using Android.Support.V7.App;
-using Android.Widget;
-using mrousavy.APIs.WebUntisSharp.WebUnitsJsonSchemes.Classes;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Android.Support.V7.App;
+using Android.App;
+using Android.OS;
+using Android.Widget;
 
 namespace SchoolTool {
-    [Activity(Theme = "@style/SchoolToolDefault", Label = "SettingsActivity")]
+    [Activity(Theme = "@style/SchoolToolDefault", Label = "SettingsTEMP")]
     public class SettingsActivity : AppCompatActivity {
         private DataManager _dataman;
-        private List<Class> _classes;
 
         protected override void OnCreate(Bundle savedInstanceState) {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.Settings);
 
-            //The switch to turn notifications on or off
-            Switch notifier = FindViewById<Switch>(Resource.Id.changeInTimetableSwitch);
-            notifier.CheckedChange += Notifier_CheckedChange;
+            //fav class selector
+            Spinner classSpinner = FindViewById<Spinner>(Resource.Id.favouriteTimetable);
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerDropDownItem);
 
-            //alarm TextView
-            TextView alarm = FindViewById<TextView>(Resource.Id.alarmTextView);
-            alarm.Click += delegate {
-                StartActivity(typeof(WeckerActivity));
-            };
+            classSpinner.Adapter = adapter;
 
-            //favourite timetable layout
-            Spinner favouriteTimetableLayout = FindViewById<Spinner>(Resource.Id.favouriteTimetable); //changed, because it's now a spinner
-            favouriteTimetableLayout.Click += FavouriteTimetableLayout_Click;
+            if (StaticWebUntis.Classes != null)
+                adapter.AddAll(new List<string>(StaticWebUntis.Classes.Select(u => u.name)));
+            else
+                Toast.MakeText(this, "Error: Could not load classes!", ToastLength.Long).Show();
 
-            //starting timetable layout
-            Spinner startingTimetableLayout = FindViewById<Spinner>(Resource.Id.startingTimetable); //changed, because it's now a spinner
-            startingTimetableLayout.Click += StartingTimetableLayout_Click;
 
+            //timetable on startup selector
+            Spinner optionSpinner = FindViewById<Spinner>(Resource.Id.startingTimetable);
+            ArrayAdapter<string> optionAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerDropDownItem);
+
+            optionSpinner.Adapter = optionAdapter;
+
+            optionAdapter.Add("Startstundenplan");
+            optionAdapter.Add("Lieblingsstundenplan");
+            optionAdapter.Add("Zuletzt angesehener");
 
             //logout button - calls Logout on Click
             Button logout = FindViewById<Button>(Resource.Id.logoutButton);
             logout.Click += Logout_Click;
 
+            //logout button - calls Logout on Click
+            TextView alarm = FindViewById<TextView>(Resource.Id.alarmTextView);
+            alarm.Click += delegate {
+                StartActivity(typeof(WeckerActivity));
+            };
+
+            //The switch to turn notifications on or off
+            Switch notifier = FindViewById<Switch>(Resource.Id.changeInTimetableSwitch);
+            notifier.CheckedChange += Notifier_CheckedChange;
+
             _dataman = new DataManager(this);
         }
+
+
 
         private void Logout_Click(object sender, EventArgs e) {
             _dataman.DeleteLogin();
             _dataman.DeleteClasses();
             StartActivity(typeof(MainActivity));
-        }
-
-        private void StartingTimetableLayout_Click(object sender, System.EventArgs e) {
-            //TODO: open picker layout
-
-            //TODO: set TextView text
-            //TextView favouriteTimetableOutput = FindViewById<TextView>(Resource.Id.favouriteTimetableOutput);
-        }
-
-        private void FavouriteTimetableLayout_Click(object sender, System.EventArgs e) {
-            //TODO: open picker layout
-
-            //TODO: set TextView text
-            //FindViewById<TextView>(Resource.Id.startingTimetableOutput);
         }
 
         private void Notifier_CheckedChange(object sender, CompoundButton.CheckedChangeEventArgs e) {
